f4c02dcf4a489b4a174c6662a80767b23c9e896f
AlekseyChirkin <ach1010110@yandex.ru>
2023-05-19 16:44:34 +0300

    add an offline speach recognition. some errors

--- stt.py
+++ stt.py
@@ -1,22 +1,46 @@
-import speech_recognition as sr
 from pydub import AudioSegment
+from vosk import Model, KaldiRecognizer
+import json
+import wave
+import os
 
 
 def audio_to_text(path_to_file: str) -> str:
-    r = sr.Recognizer()
-    path_to_wav = path_to_file[-3] + 'wav'
-
-    # # Temp
-    # path_to_wav = path_to_file
-
     # Convert .ogg to .wav
+    path_to_wav_file = path_to_file[-3] + 'wav'
     audio = AudioSegment.from_ogg(path_to_file)
-    audio.export(path_to_wav, format="wav")
-    print("File converted to .wav")
+    audio.export(path_to_wav_file, format="wav", bitrate=16000)
 
-    audio_file = sr.AudioFile(path_to_wav)
-    with audio_file as source:
-        audio = r.record(source)
+    # check vosk model exists
+    if not os.path.exists("model"):
+        print("Please download the model from https://alphacephei.com/vosk/models and unpack as 'model' in the "
+              "current folder.")
+        exit(1)
 
-    print("Recognition...")
-    return r.recognize_google(audio, language="RU-ru")
+    # Use Test file
+    # path_to_wav_file = 'C:\\temp\\aichat_tg_bot\\files\\voices\\decoder-test.wav'
+
+    recognized_data = ""
+    try:
+        wave_audio_file = wave.open(path_to_wav_file, "rb")
+        model = Model(r"model")
+        offline_recognizer = KaldiRecognizer(model, wave_audio_file.getframerate())
+
+        print('reading frames')
+        data = wave_audio_file.readframes(wave_audio_file.getnframes())
+
+        print('frames read')
+        if len(data) > 0:
+            if offline_recognizer.AcceptWaveform(data):
+                recognized_data = offline_recognizer.Result()
+
+                # получение данных распознанного текста из JSON-строки
+                # (чтобы можно было выдать по ней ответ)
+                print('получение данных распознанного текста из JSON-строки')
+                recognized_data = json.loads(recognized_data)
+                recognized_data = recognized_data["text"]
+    except:
+        print("Sorry, speech service is unavailable. Try again later")
+
+    print(recognized_data)
+    return recognized_data
