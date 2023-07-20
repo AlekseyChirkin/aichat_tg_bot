4de3174794ed5d36415f818a5b62d03bf5fea8d9
AlekseyChirkin <ach1010110@yandex.ru>
2023-07-20 16:37:58 +0300

    Not working... again... slept 2 hours... not understanding

--- bot.py
+++ bot.py
@@ -7,10 +7,7 @@ import stt
 from dotenv import load_dotenv
 import os
 from art import tprint
-
 import g4f_responce
-
-import text_from_youtube
 
 load_dotenv()
 TOKEN = os.getenv("TELEGRAM_TOKEN")
@@ -24,7 +21,7 @@ logging.basicConfig(
 )
 
 stt_obj = stt.STT()
-#print(f'Object {stt_obj=} created!')
+logging.info(f'Object {stt_obj=} created successfull!')
 tprint("Working...")
 
 
@@ -37,45 +34,39 @@ async def cmd_start(message: types.Message):
     await message.reply(f"Hi, {user_full_name}, my captain!\nWaiting for orders...")
 
 
+# На текст пользователя отвечает сразу нейросетью
 @dp.message_handler(content_types="text")
 async def echo_from_g4f(message: types.Message):
-    # Отправляем ответ нейросети
     await message.answer(g4f_responce.get_responce(message.text))
 
 
-# async def handle_file(file: File, file_name: str, path: str):
-#     Path(f"{path}").mkdir(parents=True, exist_ok=True)
-#     await bot.download_file(file_path=file.file_path, destination=f"{path}/{file_name}")
-
-
+### Здесь должена быть обработка сообщения, если к нему приложено видео с ютуб ###
 # @dp.message_handler(content_types=[ContentType.TEXT])
 # async def voice_message_handler(message: Message):
 #     text_from_user = message.text
-#
 #     if 'youtube.com/watch?v=' in text_from_user:
 #         youtube_id = text_from_user[:-11]
 #         await message.reply(text_from_youtube.transcript_from_yt_video(youtube_id))
 
-
+# Обработка голосового сообщения
 @dp.message_handler(content_types=[ContentType.VOICE])
 async def voice_message_handler(message: Message):
     voice = await message.voice.get_file()
-    path_to_save_voices = "files/voices"
+    path_voices_tmp = "files/voices"
     voice_file_name = voice.file_id + '.ogg'
-    path_to_saved_voice_file = str(path_to_save_voices) + '/' + voice_file_name
-    await handle_file(file=voice, file_name=voice_file_name, path=path_to_save_voices)
+    path_to_voice = str(path_voices_tmp) + '/' + voice_file_name
 
-    logging.info(f'File {voice_file_name} saved to {path_to_saved_voice_file} at {time.asctime()}')
-    text_from_message = stt_obj.audio_to_text(path_to_saved_voice_file)
-    
-    await message.reply(f"Распознанный текст:\n {text_from_message}")
-
-    # Получаем ответ от нейросети
-    response = g4f_responce.get_responce(text_from_message)
-    await message.answer(response)
+    # Save audio message to file
+    await handle_file(file=voice, file_name=voice_file_name, path=path_voices_tmp)
+    logging.info(f"File {voice_file_name} saved to {path_to_voice} at {time.asctime()}")
+    text_from_message = stt_obj.audio_to_text(path_to_voice)
+    logging.info(f"Распознанный текст запроса: \n{text_from_message}\n")
+    # Получаем ответ нейросети
+    # response = g4f_responce.get_responce(text_from_message)
+    # await message.reply(response)
+    await message.reply(text_from_message)
 
 if __name__ == "__main__":
-
     # polling bot
     try:
         executor.start_polling(dp, skip_updates=True)
