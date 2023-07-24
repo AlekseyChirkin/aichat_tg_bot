beca25a7194106fc84320e60817bff4a12fdbd97
AlekseyChirkin <ach1010110@yandex.ru>
2023-07-24 12:00:50 +0300

    add translating request to english and answer to russian

--- ai_llama2.py
+++ ai_llama2.py
@@ -1,6 +1,6 @@
 from ctransformers import AutoModelForCausalLM
 
-llm = AutoModelForCausalLM.from_pretrained('..\llama_models\mythoboros-13b.ggmlv3.q3_K_S.bin', model_type='llama')
+llm = AutoModelForCausalLM.from_pretrained('..\llama_models\mythoboros-13b.ggmlv3.q4_0.bin', model_type='llama')
 
 def get_answer(message: str)-> str:
     return llm(message)
--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo on
 @echo Начало работы - %DATE% в %TIME%
-C:\temp\aichat_tg_bot\venv\Scripts\python.exe bot.py
+C:\temp\chat_bot_tg\aichat_tg_bot\venv\Scripts\python.exe bot.py
 pause
