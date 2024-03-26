39f8db8785e23cc2b8353674f14271bfe2385987
Aleksei <ach1010110@yandex.ru>
2024-03-26 12:27:37 +0300

    rebuild main bot structure

--- ai_llama2.py
+++
@@ -1,6 +0,0 @@
-from ctransformers import AutoModelForCausalLM
-
-llm = AutoModelForCausalLM.from_pretrained('..\llama_models\mythoboros-13b.ggmlv3.q4_0.bin', model_type='llama')
-
-def get_answer(message: str)-> str:
-    return llm(message)
