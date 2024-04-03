054720f97d36e5bc0a15a7bc951a5319ad40d946
AlekseyChirkin <ach1010110@yandex.ru>
2024-04-03 17:55:00 +0300

    simple echo bot

--- main.py
+++ main.py
@@ -1,21 +1,32 @@
-
-from aiogram import types
-from aiogram import Dispatcher
-from aiogram import Bot
-from dotenv import load_dotenv
 import os
 import logging
 import asyncio
 
+from dotenv import load_dotenv
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram import types
+from aiogram.filters import CommandStart, Command
+
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
-
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def start_message(message: types.Message):
+    await message.answer(text=f"Hello, {message.from_user.full_name}")
+
+
+@dp.message(Command("help"))
+async def handle_help(message: types.Message):
+    text = "I'm an echo bot\nSend me any message!"
+    await message.answer(text=text)
+
+
 @dp.message()
-async def echo_message(message: types.message):
+async def echo_message(message: types.Message):
     await bot.send_message(
         chat_id=message.chat.id,
         text="Start processing..."
@@ -28,13 +39,10 @@ async def echo_message(message: types.message):
     await message.answer(
         text="Wait a second, please!"
     )
-    if message.text:
-        await message.reply(text=message.text)
-    elif message.sticker:
-        await message.reply_sticker(sticker=message.sticker.file_id)
-
-    else:
-        await message.reply(text="Something new 😀")
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text="Something new!")
 
 
 async def main():
